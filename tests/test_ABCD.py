fa77158cd73411c56e7d5385d870144d956e9429
Mason Proffitt <mason.proffitt@cern.ch>
2022-07-04 15:37:35 +0200

    make comparisons to zero consistent in tests

--- tests/test_ABCD.py
+++ tests/test_ABCD.py
@@ -68,8 +68,8 @@ def test_fixed_params():
 def test_bkg_only_fit():
     abcd = ABCD(observed_yields, signal_yields, signal_uncertainty)
     bkg_only_fit = abcd.bkg_only_fit()
-    assert bkg_only_fit[0][0] == 0
-    assert bkg_only_fit[1][0] == 0
+    assert math.isclose(bkg_only_fit[0][0], 0, abs_tol=1e-6)
+    assert math.isclose(bkg_only_fit[1][0], 0, abs_tol=1e-6)
     assert math.isclose(bkg_only_fit[2][0], observed_yields['A'], rel_tol=1e-2)
     assert math.isclose(
         bkg_only_fit[3][0],
